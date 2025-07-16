79dacea2e9af9aa866614f7a0f3ede05fcf6c146
SOUVIK SAHA <souvik21102001@gmail.com>
2025-07-16 14:45:59 +0530

    Question 1

---
+++ Q01. Recyclable and Low Fat Products.sql
@@ -0,0 +1,17 @@
+-- CREATE DATABASE SQL50;
+
+-- CREATE TABLE IF NOT EXISTS Products (
+--     product_id INT,
+--     low_fats ENUM('Y', 'N'),
+--     recyclable ENUM('Y', 'N')
+-- );
+
+-- TRUNCATE TABLE Products;
+
+-- INSERT INTO Products (product_id, low_fats, recyclable) VALUES (0, 'Y', 'N');
+-- INSERT INTO Products (product_id, low_fats, recyclable) VALUES (1, 'Y', 'Y');
+-- INSERT INTO Products (product_id, low_fats, recyclable) VALUES (2, 'N', 'Y');
+-- INSERT INTO Products (product_id, low_fats, recyclable) VALUES (3, 'Y', 'Y');
+-- INSERT INTO Products (product_id, low_fats, recyclable) VALUES (4, 'N', 'N');
+
+SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y';
